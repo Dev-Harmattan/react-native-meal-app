c4855fbc40a5210c3d2591126cc02f54cb944b2c
Sodiq Olalekan <ganiyusodiq059@gmail.com>
2022-11-05 12:58:09 -0600

    Add react native navigation

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
-import {useEffect, useCallback} from 'react';
-import { StyleSheet, View, StatusBar } from 'react-native';
-import {CategoryScreen} from './screens/CategoryScreen';
+import { useEffect, useCallback } from 'react';
+import { StyleSheet, View } from 'react-native';
+import {StatusBar} from 'expo-status-bar'
+import { CategoryScreen } from './screens/CategoryScreen';
 import { useFonts } from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-
   const [fontLoaded] = useFonts({
     'nunito-regular': require('./assets/fonts/Nunito/static/Nunito-Regular.ttf'),
     'nunito-semi-bold': require('./assets/fonts/Nunito/static/Nunito-SemiBold.ttf'),
@@ -17,24 +21,27 @@ export default function App() {
       await SplashScreen.preventAutoHideAsync();
     }
     prepare();
-  },[])
+  }, []);
 
   const onLayoutRootView = useCallback(async () => {
-    if(fontLoaded) {
+    if (fontLoaded) {
       await SplashScreen.hideAsync();
     }
   }, [fontLoaded]);
 
-  if(!fontLoaded) {
+  if (!fontLoaded) {
     return null;
   }
 
-
   return (
     <>
-    <StatusBar style='light' />
+      <StatusBar style="dark"/>
       <View style={styles.container} onLayout={onLayoutRootView}>
-        <CategoryScreen />
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen name="category" component={CategoryScreen} />
+          </Stack.Navigator>
+        </NavigationContainer>
       </View>
     </>
   );
